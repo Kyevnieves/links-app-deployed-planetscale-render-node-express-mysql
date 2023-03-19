64e9ca68c625331567aa785de65e3f5718306ace
Kyevnieves <keyvernievess@gmail.com>
2023-03-19 10:08:52 -0430

    first commit

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  database: {
+    host: "ENVIROMENT_VARIABLE",
+    user: "ENVIROMENT_VARIABLE",
+    password: "ENVIROMENT_VARIABLE",
+    database: "ENVIROMENT_VARIABLE",
+  },
+};
